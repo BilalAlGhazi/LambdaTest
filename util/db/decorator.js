8ae6a6bfa2b72e2cd702c1ea35067fe40cc4ed37
Bilal Alghazi <balghazi@gmail.com>
2020-08-24 10:42:33 -0400

    fix: build error

--- util/db/decorator.js
+++ util/db/decorator.js
@@ -1,8 +1,11 @@
-import { dynamo, schema } from 'util/dependencies';
+import { dynamo, schema } from 'util/db';
 
 export default function decorator(fn) {
   return (event, context) => {
-    return fn({ utils: { dynamo: dynamo(process.env.DYNAMO_TABLE), schema }, event, context});
-  }
+    return fn({
+      utils: { dynamo: dynamo(process.env.DYNAMO_TABLE), schema },
+      event,
+      context,
+    });
+  };
 }
-
